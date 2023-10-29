7127febf0aad00a7efbdd1f09df1e351cbcadfa9
gabriel9216 <gabrielszydlowski37@gmail.com>
2023-10-29 21:12:26 +0100

    added new changes

---
+++ script.js
@@ -0,0 +1,26 @@
+function check(number) {
+    if(number=== "" || isNaN(number)) {
+        alert("Prosze wpisać wartość liczbowa");
+        return false;
+    } else
+    return true;
+}
+function celcjusz()
+{
+	let number=document.getElementById('number').value;
+	let celcjusz=number*'1.8'+'32';
+	let result=document.getElementById('result');
+	if(check(number)){
+		result.innerHTML=number+" stopnie celcjusza to "+celcjusz+" fehrenheitów";
+	}
+
+}
+function fahrenheit()
+{
+	let number=document.getElementById('number').value;
+	let fahrenheit=(number-'32')/'1.8';
+	let result=document.getElementById('result');
+	if(check(number)){
+			result.innerHTML=number+" fehrenheity to "+fahrenheit+" stopieni celcjusza";
+	}
+}
